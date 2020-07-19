c1b122a73b190a9c170f5ac416b90a22ac71d47d
Px Mx <foospidy@users.noreply.github.com>
2020-07-19 15:38:27 -0400

    Add Tiny-XSS-Payloads

--- get.sh
+++ get.sh
@@ -180,6 +180,12 @@ unzip master.zip
 rm master.zip
 mv web-cve-tests-master web-cve-tests
 
+### terjanq
+wget https://github.com/terjanq/Tiny-XSS-Payloads/archive/master.zip
+unzip master.zip
+rm master.zip
+mv Tiny-XSS-Payloads-master Tiny-XSS-Payloads
+
 ###### Uncompress these files
 echo Extracting compressed payload files...
 
